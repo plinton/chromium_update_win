b94dcf879e82fea7868b092c482a98f91c019167
Paul Linton <plinton@wesleyan.edu>
2012-11-22 10:59:42 -0800

    use new urls

--- update_chromium.py
+++ update_chromium.py
@@ -1,9 +1,10 @@
 import urllib2, subprocess, tempfile
 
-url = "http://build.chromium.org/f/chromium/snapshots/Win_Webkit_Latest/"
-ver_file = urllib2.urlopen(url + "LATEST")
+url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/"
+ver_file = urllib2.urlopen(url + "LAST_CHANGE")
 ver = ver_file.read()
 print("getting version " + ver)
+installer_url = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?path=Win"
 installer_file = urllib2.urlopen(url + ver + "/mini_installer.exe")
 temp_folder = tempfile.mkdtemp()
 temp_file = open(temp_folder + "mini_installer.exe", "w+b")
